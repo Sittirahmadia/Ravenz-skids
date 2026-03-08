d2a8d0b8bd4815d513c161050eb219aac83bcd95
Sittirahmadia <sittirahmadia67@gmail.com>
2026-03-08 12:06:03 +0800

    fix: offHand -> getOffHandStack()

--- src/main/java/com/raven/ravenz/module/modules/combat/AutoCrystal.java
+++ src/main/java/com/raven/ravenz/module/modules/combat/AutoCrystal.java
@@ -1,141 +1,180 @@
 package com.raven.ravenz.module.modules.combat;
 
-import com.raven.ravenz.event.impl.input.HandleInputEvent;
+import com.raven.ravenz.event.impl.player.AttackEvent;
+import com.raven.ravenz.event.impl.player.ItemUseEvent;
 import com.raven.ravenz.event.impl.player.TickEvent;
-import com.raven.ravenz.mixin.MinecraftClientAccessor;
 import com.raven.ravenz.module.Category;
 import com.raven.ravenz.module.Module;
 import com.raven.ravenz.module.setting.BooleanSetting;
-import com.raven.ravenz.module.setting.KeybindSetting;
 import com.raven.ravenz.module.setting.NumberSetting;
-import com.raven.ravenz.utils.keybinding.KeyUtils;
-import com.raven.ravenz.utils.math.TimerUtil;
-import com.raven.ravenz.utils.mc.InventoryUtil;
+import com.raven.ravenz.utils.keybinding.simulation.ClickSimulator;
 import meteordevelopment.orbit.EventHandler;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.decoration.EndCrystalEntity;
 import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 import org.lwjgl.glfw.GLFW;
 
-/**
- * @author koi
- */
 public final class AutoCrystal extends Module {
 
-    private final KeybindSetting crystalKey = new KeybindSetting("Crystal Key", GLFW.GLFW_MOUSE_BUTTON_4, false);
+    // Place settings
+    private final NumberSetting placeDelay = new NumberSetting("Place Delay", 0, 20, 0, 1);
+    private final NumberSetting placeChance = new NumberSetting("Place Chance", 0, 100, 100, 1);
 
-    private final NumberSetting delay = new NumberSetting("Delay (MS)", 1, 200, 50, 1);
+    // Break settings
+    private final NumberSetting breakDelay = new NumberSetting("Break Delay", 0, 20, 0, 1);
+    private final NumberSetting breakChance = new NumberSetting("Break Chance", 0, 100, 100, 1);
 
-    private final BooleanSetting antiSuicide = new BooleanSetting("Anti Suicide", true);
+    // Behaviour settings
+    private final BooleanSetting onRmb = new BooleanSetting("On RMB", true);
+    private final BooleanSetting fastMode = new BooleanSetting("Fast Mode", true);
+    private final BooleanSetting clickSimulation = new BooleanSetting("Click Simulation", false);
+    private final BooleanSetting noCountGlitch = new BooleanSetting("No Count Glitch", true);
+    private final BooleanSetting noBounce = new BooleanSetting("No Bounce", true);
+    private final BooleanSetting autoSwitch = new BooleanSetting("Auto Switch", false);
+    private final BooleanSetting requireObsidian = new BooleanSetting("Require Obsidian", true);
 
-    private final BooleanSetting autoSwitch = new BooleanSetting("Auto Switch", true);
-
-    private final BooleanSetting switchBack = new BooleanSetting("Switch Back", true);
-
-    private final BooleanSetting antiWeakness = new BooleanSetting("Anti Weakness", true);
-
-    private final TimerUtil timer = new TimerUtil();
-
-    private int originalSlot = -1;
+    private int placeClock = 0;
+    private int breakClock = 0;
+    private EndCrystalEntity lastBrokenCrystal = null;
 
     public AutoCrystal() {
-        super("Auto Crystal", "Hold key to spam crystals", -1, Category.COMBAT);
-        this.addSettings(crystalKey, delay, antiSuicide, autoSwitch, switchBack, antiWeakness);
-        this.getSettings().removeIf(setting -> setting instanceof KeybindSetting && !setting.equals(crystalKey));
-    }
-
-    @EventHandler
-    private void onTickEvent(HandleInputEvent event) {
-        if (isNull()) return;
-        if (mc.currentScreen != null) return;
-
-        if (!KeyUtils.isKeyPressed(crystalKey.getKeyCode())) return;
-
-        if (timer.hasElapsedTime(delay.getValueInt())) {
-            processCrystal();
-            timer.reset();
-        }
-    }
-
-    private void processCrystal() {
-        if (antiSuicide.getValue() && !mc.player.isOnGround()) return;
-        if (mc.crosshairTarget instanceof EntityHitResult entityHit) {
-            if (entityHit.getEntity() instanceof EndCrystalEntity crystal) {
-                if (!crystal.isRemoved() && crystal.isAlive() && mc.world.getEntityById(crystal.getId()) != null) {
-
-                    if (mc.player.getEntityPos().distanceTo(crystal.getEntityPos()) <= 4.5) {
-                        if (antiWeakness.getValue() && mc.player.hasStatusEffect(net.minecraft.entity.effect.StatusEffects.WEAKNESS)) {
-                            InventoryUtil.swapToSword();
-                        }
-                        ((MinecraftClientAccessor) mc).invokeDoAttack();
-                    }
-                }
-                return;
-            }
-        }
-
-        if (mc.crosshairTarget instanceof BlockHitResult blockHit) {
-            BlockPos targetBlock = blockHit.getBlockPos();
-            BlockPos placementPos = targetBlock.offset(blockHit.getSide());
-
-            if (isObsidianOrBedrock(targetBlock) && isValidCrystalPosition(placementPos)) {
-                if (autoSwitch.getValue() && hasItemInHotbar()) {
-                    InventoryUtil.swapToSlot(Items.END_CRYSTAL);
-                }
-
-                if (mc.player.getMainHandStack().getItem() == Items.END_CRYSTAL) {
-                    ((MinecraftClientAccessor) mc).invokeDoItemUse();
-                }
-            }
-        }
-    }
-
-    private boolean hasItemInHotbar() {
-        for (int i = 0; i < 9; i++) {
-            var stack = mc.player.getInventory().getStack(i);
-            if (!stack.isEmpty() && stack.getItem() == Items.END_CRYSTAL) return true;
-        }
-        return false;
-    }
-
-    private boolean isObsidianOrBedrock(BlockPos pos) {
-        if (mc.world == null) return false;
-        var block = mc.world.getBlockState(pos).getBlock();
-        return block == net.minecraft.block.Blocks.OBSIDIAN || block == net.minecraft.block.Blocks.BEDROCK;
-    }
-
-    private boolean isValidCrystalPosition(BlockPos pos) {
-        if (mc.world == null) return false;
-        if (mc.player.getEntityPos().distanceTo(Vec3d.ofCenter(pos)) > 4.5) return false;
-
-        if (!mc.world.getBlockState(pos).isAir()) return false;
-        if (!mc.world.getBlockState(pos.up()).isAir()) return false;
-
-        BlockPos playerPos = mc.player.getBlockPos();
-        return !pos.equals(playerPos) && !pos.equals(playerPos.up());
+        super("Auto Crystal", "Automatically places and breaks end crystals", -1, Category.COMBAT);
+        addSettings(placeDelay, placeChance, breakDelay, breakChance,
+                onRmb, fastMode, clickSimulation, noCountGlitch, noBounce,
+                autoSwitch, requireObsidian);
     }
 
     @Override
     public void onEnable() {
+        reset();
         super.onEnable();
-        if (autoSwitch.getValue()) originalSlot = mc.player.getInventory().selectedSlot;
-        timer.reset();
     }
 
-    @Override
-    public void onDisable() {
-        super.onDisable();
-        if (switchBack.getValue() && originalSlot != -1) {
-            mc.player.getInventory().selectedSlot = originalSlot;
+    @EventHandler
+    private void onTick(TickEvent event) {
+        if (isNull() || mc.currentScreen != null) return;
+
+        if (onRmb.getValue() &&
+                GLFW.glfwGetMouseButton(mc.getWindow().getHandle(), GLFW.GLFW_MOUSE_BUTTON_RIGHT) != GLFW.GLFW_PRESS)
+            return;
+
+        if (autoSwitch.getValue() && !mc.player.getMainHandStack().isOf(Items.END_CRYSTAL)) {
+            for (int i = 0; i < 9; i++) {
+                if (mc.player.getInventory().getStack(i).isOf(Items.END_CRYSTAL)) {
+                    mc.player.getInventory().selectedSlot = i;
+                    break;
+                }
+            }
         }
-        originalSlot = -1;
+
+        if (mc.player.getMainHandStack().isOf(Items.END_CRYSTAL)) {
+            placeCrystal();
+            breakCrystal();
+        }
+
+        placeClock++;
+        breakClock++;
     }
 
-    @Override
-    public int getKey() {
-        return -1;
+    private void placeCrystal() {
+        if (placeClock < placeDelay.getValueInt()) return;
+        if (Math.random() * 100 > placeChance.getValue()) return;
+
+        var crosshair = mc.crosshairTarget;
+
+        if (crosshair instanceof BlockHitResult blockHit && blockHit.getType() != HitResult.Type.MISS) {
+            BlockPos pos = blockHit.getBlockPos();
+            var state = mc.world.getBlockState(pos);
+
+            boolean validBlock = !requireObsidian.getValue()
+                    || state.isOf(Blocks.OBSIDIAN)
+                    || state.isOf(Blocks.BEDROCK);
+
+            if (validBlock && canPlaceCrystal(pos)) {
+                doPlace(blockHit);
+                placeClock = 0;
+            }
+
+        } else if (fastMode.getValue() && crosshair instanceof EntityHitResult entityHit
+                && entityHit.getEntity() instanceof EndCrystalEntity crystal) {
+            tryFastPlace(crystal);
+        }
     }
-}// reverted
+
+    private void tryFastPlace(EndCrystalEntity crystal) {
+        BlockPos below = crystal.getBlockPos().down();
+        var state = mc.world.getBlockState(below);
+        if (!state.isOf(Blocks.OBSIDIAN) && !state.isOf(Blocks.BEDROCK)) return;
+        if (!canPlaceCrystal(below)) return;
+
+        var blockHit = new BlockHitResult(
+                new net.minecraft.util.math.Vec3d(below.getX() + 0.5, below.getY() + 1.0, below.getZ() + 0.5),
+                net.minecraft.util.math.Direction.UP, below, false);
+        doPlace(blockHit);
+        placeClock = 0;
+    }
+
+    private void doPlace(BlockHitResult blockHit) {
+        if (clickSimulation.getValue()) ClickSimulator.leftClick();
+        ActionResult res = mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, blockHit);
+        if (res.isAccepted()) mc.player.swingHand(Hand.MAIN_HAND);
+    }
+
+    private void breakCrystal() {
+        if (breakClock < breakDelay.getValueInt()) return;
+        if (Math.random() * 100 > breakChance.getValue()) return;
+        if (!(mc.crosshairTarget instanceof EntityHitResult hit)) return;
+        if (!(hit.getEntity() instanceof EndCrystalEntity crystal)) return;
+        if (crystal == lastBrokenCrystal) return;
+        doBreak(crystal);
+    }
+
+    private void doBreak(EndCrystalEntity crystal) {
+        if (clickSimulation.getValue()) ClickSimulator.leftClick();
+        mc.interactionManager.attackEntity(mc.player, crystal);
+        mc.player.swingHand(Hand.MAIN_HAND);
+        lastBrokenCrystal = crystal;
+        breakClock = 0;
+    }
+
+    private boolean canPlaceCrystal(BlockPos pos) {
+        BlockPos above = pos.up();
+        BlockPos above2 = pos.up(2);
+        if (!mc.world.getBlockState(above).isAir()) return false;
+        if (!mc.world.getBlockState(above2).isAir()) return false;
+        var box = new net.minecraft.util.math.Box(
+                above.getX(), above.getY(), above.getZ(),
+                above.getX() + 1, above.getY() + 2, above.getZ() + 1);
+        return mc.world.getOtherEntities(null, box).isEmpty();
+    }
+
+    private void reset() {
+        placeClock = placeDelay.getValueInt();
+        breakClock = breakDelay.getValueInt();
+        lastBrokenCrystal = null;
+    }
+
+    @EventHandler
+    private void onItemUse(ItemUseEvent event) {
+        if (!mc.player.getMainHandStack().isOf(Items.END_CRYSTAL)) return;
+        if (noCountGlitch.getValue() &&
+                GLFW.glfwGetMouseButton(mc.getWindow().getHandle(), GLFW.GLFW_MOUSE_BUTTON_RIGHT) != GLFW.GLFW_PRESS) {
+            event.cancel();
+        }
+    }
+
+    @EventHandler
+    private void onAttack(AttackEvent event) {
+        if (!mc.player.getMainHandStack().isOf(Items.END_CRYSTAL)) return;
+        if (noBounce.getValue() &&
+                GLFW.glfwGetMouseButton(mc.getWindow().getHandle(), GLFW.GLFW_MOUSE_BUTTON_LEFT) != GLFW.GLFW_PRESS) {
+            event.cancel();
+        }
+    }
+}
--- src/main/java/com/raven/ravenz/module/modules/combat/AutoDtap.java
+++ src/main/java/com/raven/ravenz/module/modules/combat/AutoDtap.java
@@ -1,9 +1,8 @@
 package com.raven.ravenz.module.modules.combat;
 
-import com.raven.ravenz.event.impl.input.HandleInputEvent;
 import com.raven.ravenz.event.impl.player.AttackEvent;
 import com.raven.ravenz.event.impl.player.ItemUseEvent;
-import com.raven.ravenz.mixin.MinecraftClientAccessor;
+import com.raven.ravenz.event.impl.player.TickEvent;
 import com.raven.ravenz.module.Category;
 import com.raven.ravenz.module.Module;
 import com.raven.ravenz.module.setting.BooleanSetting;
@@ -11,232 +10,146 @@ import com.raven.ravenz.module.setting.NumberSetting;
 import com.raven.ravenz.utils.keybinding.simulation.ClickSimulator;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.block.Blocks;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.decoration.EndCrystalEntity;
 import net.minecraft.item.Items;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 import org.lwjgl.glfw.GLFW;
-
-import java.util.List;
 
 /**
  * AutoDtap — crosshair-based double-pop crystal placer.
- *
- * Watches mc.crosshairTarget each tick:
- *   - EntityHitResult → break the crystal (doBreak)
- *   - BlockHitResult  → place a crystal (doPlace)
- *
- * Place validation uses an inline canPlace() that checks:
- *   - block is obsidian or bedrock (when requireObsidian is on)
- *   - the block above the placement face is air (and the one above that)
- *   - player is within 4.5 blocks of the placement pos
- *
- * This mirrors KeyCrystal's proven placement logic to avoid any
- * "canPlaceCrystalServer" || bug found in other clients.
+ * Uses canPlaceCrystal() with inline obsidian/bedrock check to avoid
+ * the broken canPlaceCrystalServer bug (uses && instead of ||).
  */
 public final class AutoDtap extends Module {
 
-    // ── Settings ───────────────────────────────────────────────────────────
-
-    private final NumberSetting placeDelay = new NumberSetting(
-            "Place Delay (ticks)", 0, 20, 0, 1);
-
-    private final NumberSetting breakDelay = new NumberSetting(
-            "Break Delay (ticks)", 0, 20, 0, 1);
-
-    private final NumberSetting maxCrystals = new NumberSetting(
-            "Max Crystals", 1, 6, 2, 1);
-
-    private final BooleanSetting requireObsidian = new BooleanSetting(
-            "Require Obsidian", true);
-
-    private final BooleanSetting simulateClick = new BooleanSetting(
-            "Simulate Click", true);
-
-    private final BooleanSetting noCountGlitch = new BooleanSetting(
-            "No Count Glitch", true);
-
-    private final BooleanSetting noBounce = new BooleanSetting(
-            "No Bounce", true);
-
-    private final BooleanSetting activateOnRightClick = new BooleanSetting(
-            "Activate On RightClick", false);
-
-    private final BooleanSetting stopOnKill = new BooleanSetting(
-            "Stop On Kill", false);
-
-    // ── State ──────────────────────────────────────────────────────────────
+    private final NumberSetting  placeDelay          = new NumberSetting("Place Delay",         0, 20, 0, 1);
+    private final NumberSetting  breakDelay          = new NumberSetting("Break Delay",          0, 20, 0, 1);
+    private final NumberSetting  maxCrystals         = new NumberSetting("Max Crystals",         1, 6,  2, 1);
+    private final BooleanSetting requireObsidian     = new BooleanSetting("Require Obsidian",    true);
+    private final BooleanSetting simulateClick       = new BooleanSetting("Simulate Click",      true);
+    private final BooleanSetting noCountGlitch       = new BooleanSetting("No Count Glitch",     true);
+    private final BooleanSetting noBounce            = new BooleanSetting("No Bounce",           true);
+    private final BooleanSetting activateOnRightClick= new BooleanSetting("Activate On RMB",     false);
 
     private int placeClock = 0;
     private int breakClock = 0;
     private int placedCount = 0;
     private EndCrystalEntity lastBrokenCrystal = null;
 
-    // ── Constructor ────────────────────────────────────────────────────────
-
     public AutoDtap() {
         super("Auto Dtap", "Crosshair-based double-pop crystal placer", -1, Category.COMBAT);
-        addSettings(
-                placeDelay, breakDelay, maxCrystals,
+        addSettings(placeDelay, breakDelay, maxCrystals,
                 requireObsidian, simulateClick, noCountGlitch, noBounce,
-                activateOnRightClick, stopOnKill
-        );
+                activateOnRightClick);
     }
-
-    // ── Lifecycle ──────────────────────────────────────────────────────────
 
     @Override
     public void onEnable() {
-        super.onEnable();
         reset();
+        super.onEnable();
     }
 
     @Override
     public void onDisable() {
-        super.onDisable();
         reset();
+        super.onDisable();
     }
 
     private void reset() {
-        placeClock        = 0;
-        breakClock        = 0;
-        placedCount       = 0;
+        placeClock = 0;
+        breakClock = 0;
+        placedCount = 0;
         lastBrokenCrystal = null;
     }
 
-    // ── Place / Break ──────────────────────────────────────────────────────
+    @EventHandler
+    private void onTick(TickEvent event) {
+        if (isNull() || mc.currentScreen != null) return;
 
-    /**
-     * Breaks the crystal under the crosshair.
-     * Mirrors KeyCrystal: optionally left-click simulate then invokeDoAttack.
-     */
+        if (activateOnRightClick.getValue() &&
+                GLFW.glfwGetMouseButton(mc.getWindow().getHandle(), GLFW.GLFW_MOUSE_BUTTON_RIGHT) != GLFW.GLFW_PRESS)
+            return;
+
+        if (!mc.player.getMainHandStack().isOf(Items.END_CRYSTAL)) return;
+
+        placeClock++;
+        breakClock++;
+
+        // Break
+        if (breakClock >= breakDelay.getValueInt()
+                && mc.crosshairTarget instanceof EntityHitResult hit
+                && hit.getEntity() instanceof EndCrystalEntity crystal
+                && crystal != lastBrokenCrystal) {
+            doBreak(crystal);
+        }
+
+        // Place
+        if (placeClock >= placeDelay.getValueInt()
+                && placedCount < maxCrystals.getValueInt()
+                && mc.crosshairTarget instanceof BlockHitResult blockHit
+                && blockHit.getType() != HitResult.Type.MISS
+                && canPlace(blockHit)) {
+            doPlace(blockHit);
+        }
+    }
+
+    private void doPlace(BlockHitResult hit) {
+        if (simulateClick.getValue()) ClickSimulator.leftClick();
+        ActionResult res = mc.interactionManager.interactBlock(mc.player, Hand.MAIN_HAND, hit);
+        if (res.isAccepted()) mc.player.swingHand(Hand.MAIN_HAND);
+        placedCount++;
+        placeClock = 0;
+    }
+
     private void doBreak(EndCrystalEntity crystal) {
         if (simulateClick.getValue()) ClickSimulator.leftClick();
-        ((MinecraftClientAccessor) mc).invokeDoAttack();
-        mc.player.swingHand(net.minecraft.util.Hand.MAIN_HAND);
+        mc.interactionManager.attackEntity(mc.player, crystal);
+        mc.player.swingHand(Hand.MAIN_HAND);
         if (placedCount > 0) placedCount--;
         lastBrokenCrystal = crystal;
         breakClock = 0;
     }
 
-    /**
-     * Places a crystal at the block the crosshair is aimed at.
-     * Mirrors KeyCrystal: invokeDoItemUse().
-     */
-    private void doPlace() {
-        ((MinecraftClientAccessor) mc).invokeDoItemUse();
-        placedCount++;
-        placeClock = 0;
-    }
-
-    /**
-     * Validates whether a crystal can be placed at the crosshair block hit.
-     *
-     * targetBlock  = the block the crosshair ray hit (must be obsidian/bedrock).
-     * placementPos = the block on the face side (must be air, with air above it).
-     */
     private boolean canPlace(BlockHitResult hit) {
-        BlockPos targetBlock   = hit.getBlockPos();
-        BlockPos placementPos  = targetBlock.offset(hit.getSide());
+        BlockPos pos = hit.getBlockPos();
+        var state = mc.world.getBlockState(pos);
 
-        // Base block must be obsidian or bedrock (when setting is on)
-        if (requireObsidian.getValue()) {
-            var block = mc.world.getBlockState(targetBlock).getBlock();
-            if (block != Blocks.OBSIDIAN && block != Blocks.BEDROCK) return false;
-        }
+        boolean validBase = !requireObsidian.getValue()
+                || state.isOf(Blocks.OBSIDIAN)
+                || state.isOf(Blocks.BEDROCK);
+        if (!validBase) return false;
 
-        // Placement column must be in range
-        if (mc.player.getEntityPos().distanceTo(Vec3d.ofCenter(placementPos)) > 4.5) return false;
+        BlockPos above = pos.up();
+        BlockPos above2 = pos.up(2);
+        if (!mc.world.getBlockState(above).isAir()) return false;
+        if (!mc.world.getBlockState(above2).isAir()) return false;
 
-        // Placement and the block above must both be air
-        if (!mc.world.getBlockState(placementPos).isAir()) return false;
-        if (!mc.world.getBlockState(placementPos.up()).isAir()) return false;
-
-        // Must not overlap the player
-        BlockPos playerPos = mc.player.getBlockPos();
-        return !placementPos.equals(playerPos)
-                && !placementPos.equals(playerPos.up())
-                && !placementPos.up().equals(playerPos)
-                && !placementPos.up().equals(playerPos.up());
+        var box = new net.minecraft.util.math.Box(
+                above.getX(), above.getY(), above.getZ(),
+                above.getX() + 1, above.getY() + 2, above.getZ() + 1);
+        return mc.world.getOtherEntities(null, box).isEmpty();
     }
 
-    // ── Tick ───────────────────────────────────────────────────────────────
-
-    @EventHandler
-    private void onHandleInput(HandleInputEvent event) {
-        if (isNull() || mc.currentScreen != null) return;
-
-        // Right-click gate
-        if (activateOnRightClick.getValue()
-                && GLFW.glfwGetMouseButton(mc.getWindow().getHandle(), GLFW.GLFW_MOUSE_BUTTON_RIGHT) != GLFW.GLFW_PRESS)
-            return;
-
-        // Must be holding an end crystal
-        if (!mc.player.isHolding(Items.END_CRYSTAL)) return;
-
-        // Stop on nearby kill
-        if (stopOnKill.getValue() && isDeadPlayerNearby()) return;
-
-        placeClock++;
-        breakClock++;
-
-        // ── Break ──────────────────────────────────────────────────────────
-        if (breakClock >= breakDelay.getValueInt()
-                && mc.crosshairTarget instanceof EntityHitResult entityHit
-                && entityHit.getEntity() instanceof EndCrystalEntity crystal
-                && crystal != lastBrokenCrystal) {
-            doBreak(crystal);
-        }
-
-        // ── Place ──────────────────────────────────────────────────────────
-        if (placeClock >= placeDelay.getValueInt()
-                && placedCount < maxCrystals.getValueInt()
-                && mc.crosshairTarget instanceof BlockHitResult blockHit
-                && blockHit.getType() == HitResult.Type.BLOCK
-                && canPlace(blockHit)) {
-            doPlace();
-        }
-    }
-
-    // ── Item use / attack cancel ───────────────────────────────────────────
-
-    /** Cancels vanilla right-click crystal-use desync when not pressing RMB. */
     @EventHandler
     private void onItemUse(ItemUseEvent event) {
-        if (isNull() || !mc.player.isHolding(Items.END_CRYSTAL)) return;
-        if (noCountGlitch.getValue()
-                && GLFW.glfwGetMouseButton(mc.getWindow().getHandle(), GLFW.GLFW_MOUSE_BUTTON_RIGHT) != GLFW.GLFW_PRESS) {
+        if (!mc.player.getMainHandStack().isOf(Items.END_CRYSTAL)) return;
+        if (noCountGlitch.getValue() &&
+                GLFW.glfwGetMouseButton(mc.getWindow().getHandle(), GLFW.GLFW_MOUSE_BUTTON_RIGHT) != GLFW.GLFW_PRESS) {
             event.cancel();
         }
     }
 
-    /** Cancels vanilla left-click attack bounce when not pressing LMB. */
     @EventHandler
     private void onAttack(AttackEvent event) {
-        if (isNull() || !mc.player.isHolding(Items.END_CRYSTAL)) return;
-        if (noBounce.getValue()
-                && GLFW.glfwGetMouseButton(mc.getWindow().getHandle(), GLFW.GLFW_MOUSE_BUTTON_LEFT) != GLFW.GLFW_PRESS) {
+        if (!mc.player.getMainHandStack().isOf(Items.END_CRYSTAL)) return;
+        if (noBounce.getValue() &&
+                GLFW.glfwGetMouseButton(mc.getWindow().getHandle(), GLFW.GLFW_MOUSE_BUTTON_LEFT) != GLFW.GLFW_PRESS) {
             event.cancel();
         }
-    }
-
-    // ── Helpers ────────────────────────────────────────────────────────────
-
-    private boolean isDeadPlayerNearby() {
-        if (mc.world == null) return false;
-        List<? extends Entity> players = mc.world.getPlayers();
-        for (Entity e : players) {
-            if (e == mc.player) continue;
-            if (e instanceof LivingEntity living && living.getHealth() <= 0f
-                    && e.squaredDistanceTo(mc.player) < 36) {
-                return true;
-            }
-        }
-        return false;
     }
 }
